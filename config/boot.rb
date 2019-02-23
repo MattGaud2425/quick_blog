f059078d4499607dcefc1bbf3b4968a48e0e486d
Matthew Gaudio <mattgaud2425@Gmail.com>
2019-02-23 03:04:35 -0500

    blog

---
+++ config/boot.rb
@@ -0,0 +1,15 @@
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+require 'rails/command'
+require 'rails/commands/server/server_command'
+module Rails
+  class Server
+    alias :_default_options :default_options
+    def default_options
+      _default_options.merge!(Host:'0.0.0.0')
+    end
+  end
+end
---
+++ vendor/bundle/ruby/2.6.0/gems/ffi-1.10.0/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/matthewgaudio/Desktop/dev/ruby/projects/quick_blog/vendor/bundle/ruby/2.6.0/gems/ffi-1.10.0/ext/ffi_c/libffi/src/x86/ffitarget.h
